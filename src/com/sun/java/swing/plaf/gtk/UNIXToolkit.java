e118e89070c7dd12f91028df55f7a5e01ff98f47
huangzhonglin <Hzl19970712>
2020-04-17 14:56:40 +0800

    com

---
+++ src/com/sun/java/swing/plaf/gtk/UNIXToolkit.java
@@ -0,0 +1,13 @@
+package com.sun.java.swing.plaf.gtk;
+
+import java.awt.*;
+
+public class UNIXToolkit {
+
+
+    public static Object GTK_LOCK;
+
+    public Image getStockIcon(int widgetType, String key, int type, int ordinal, Object o) {
+        return null;
+    }
+}
